6e20a0d8331c1aa606afe735f9375234539c4e54
Alex Kotishevskiy <alexkotishevskiy@MacBook-Air-Alex.local>
2017-03-05 16:53:53 +0300

    1st part of storyboard

---
+++ Lesson2.2/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Lesson2.2
+//
+//  Created by Alex Kotishevskiy on 05/03/2017.
+//  Copyright © 2017 Alex Kotishevskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
